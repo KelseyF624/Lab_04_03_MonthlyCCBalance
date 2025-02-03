61feff90cc6e798b1a2b344f4a7d1d01d66668e4
KelseyF624 <kelseyf.624@gmail.com>
2025-02-03 12:11:27 -0500

    Initial commit

---
+++ src/MonthyBalance.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MonthlyBalance{
+    public static void main(String[] args) {
+        int CREDIT_BALANCE = 5000;
+        double interestRate = .17;
+        double sum = interestRate * CREDIT_BALANCE;
+        System.out.println("Monthly balance is $" + sum);
+        }
+    }
