e5b570c6b410c36f5bf4b47d875b467769dcf864
KelseyF624 <kelseyf.624@gmail.com>
2025-02-03 12:13:31 -0500

    Complete Commit

--- src/MonthyBalance.java
+++ src/MonthyBalance.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class MonthlyBalance{
+public class MonthyBalance {
     public static void main(String[] args) {
         int CREDIT_BALANCE = 5000;
         double interestRate = .17;
